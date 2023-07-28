e7281f117e948cdb641aad51806a306ac62d26f7
Regina Pompeo <gina.celia.pompeo@gmail.com>
2023-07-28 17:02:36 -0300

    Adição da tela de definição do trello
    Atualizei a tela scrum e adicionei a tela definição do trello.

---
+++ teste/src/paginas/DefinicaoTrello/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import DefinicaoTrello from '../../components/img/QuadroTrello.png'; // Importe a imagem aqui
+import './styled.css';
+
+function DefinicaoComponent() {
+    return (
+        <>
+            <div className='hero'>
+                <section className="about" id="about">
+                    <h2 className="title">O que é o Trello?</h2>
+                    <div className="about-content">
+                        <div className="column">
+                            <div className="img-container">
+                                <img src={DefinicaoTrello} alt="" className="img-def" />
+                            </div>
+                            <div className="text-container">
+                                <div className="text-right-image">
+                                    O Trello é uma ferramenta de gerenciamento de projetos ou colaboração que contribui para o sucesso dos negócios e pela organização pessoal. 
+                                    Ele permite você organizar suas tarefas em boards, além de poder criar um quadro e atribuir a cada membro de sua equipe.
+                                    <br />
+                                    <br />
+                                    Como todos os projetos estarão em andamento simultaneamente, será possível gerenciá-los bem e ficar de olho no estágio de trabalho de sua equipe. 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
+}
+
+export default DefinicaoComponent;
--- teste/src/paginas/Scrum/index.js
+++ teste/src/paginas/Scrum/index.js
@@ -5,22 +5,23 @@ import './styled.css';
 function ScrumComponent() {
     return (
         <>
-            <div className='hero' >
+            <div className='hero'>
                 <section className="about" id="about">
                     <h2 className="title">Metodologia Ágil Scrum</h2>
-                    <div class="about-content">
-                        <div class="column center">
-                            <img src={Scrum} alt="" />
-                        </div>
-                        <div class="column under">
-                            <div class="text">
-                                {" "}
-                                Hoje a equipe de TI vai apresentar a ferramenta Trello com o intuito de explicar e passar
-                                informação sobre o uso dessa ferramenta para a organização profissional e pessoal.
-                            </div>
+                    <div className="about-content">
+                        <div className="column center">
+                            <img src={Scrum} alt="" className="img-scrum" />
                         </div>
                     </div>
                 </section>
+                {/* Div para inserir texto abaixo da imagem */}
+                <div className="text-below-image">
+                    A Metodologia Scrum é considerada um método ágil. Trata-se de um framework (conjunto de ações e estratégias para solucionar problemas) normalmente utilizado com projetos mais complexos. 
+                    <br />
+                    <br />
+                    O conceito principal é a ideia de finalizar um projeto mais rápido, com melhor qualidade, otimizando os recursos humanos e materiais. Além disso, potencializa o trabalho em equipe com foco no cumprimento dos prazos estipulados por meio do acompanhamento da evolução do projeto.
+
+                </div>
             </div>
         </>
     );
